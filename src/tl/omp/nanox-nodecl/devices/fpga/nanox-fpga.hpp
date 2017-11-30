f70c5b2ceb96ac32da32d05e9335422e1d7a3793
Jaume Bosch <jbosch@bsc.es>
2017-11-30 09:24:21 +0100

    Use a hash to identify fpga accelerator type
    Deprecate the use of onto(type) as type is generated hashing the source file path and function name

--- src/tl/omp/nanox-nodecl/devices/fpga/nanox-fpga.hpp
+++ src/tl/omp/nanox-nodecl/devices/fpga/nanox-fpga.hpp
@@ -1,23 +1,23 @@
 /*--------------------------------------------------------------------
   (C) Copyright 2006-2013 Barcelona Supercomputing Center
                           Centro Nacional de Supercomputacion
-  
+
   This file is part of Mercurium C/C++ source-to-source compiler.
-  
+
   See AUTHORS file in the top level directory for information
   regarding developers and contributors.
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 3 of the License, or (at your option) any later version.
-  
+
   Mercurium C/C++ source-to-source compiler is distributed in the hope
   that it will be useful, but WITHOUT ANY WARRANTY; without even the
   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
   PURPOSE.  See the GNU Lesser General Public License for more
   details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with Mercurium C/C++ source-to-source compiler; if
   not, write to the Free Software Foundation, Inc., 675 Mass Ave,
@@ -85,7 +85,7 @@ namespace TL
                 std::string _vivado_project_name;
                 std::string _ip_cache_path;
                 std::string _dataflow;
-                std::string _acc_num;
+                std::string _acc_type;
                 std::string _num_acc_instances;
                 int _current_base_acc_num;
 
